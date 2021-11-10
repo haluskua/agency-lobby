62989a366cbf1c3b4b1c6c4cff17b602c578f2b1
haluskua <haluskua.one@gmail.com>
2021-11-11 00:11:33 +1300

    added image album

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,22 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Services.module.css";
 
-import Link from 'next/link'
-import styles  from '../styles/Services.module.css'
-const Services = ({services}) => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.info}>
-            <h1 className={styles.title}>What I Can Do?</h1>
-            <h1 className={styles.subtitle}>Services I can help you with</h1>
-            <div className={styles.services}>
-                {services.map((service)=>(
-                    <Link key={service.id} href ={`/products/${service.name}`}>
-                    {service.title}
-                    </Link>
-                ))}
-            </div>
-            </div>
+const Services = ({ services }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h1 className={styles.title}>What I Can Do?</h1>
+        <h1 className={styles.subtitle}>Services I can help you with</h1>
+        <div className={styles.services}>
+          {services.map((service) => (
+            <Link passHref key={service.id} href={`/products/${service.name}`}>
+              <div className={styles.service}>
+                <span className={styles.cat}>{service.title}</span>
+                <div className={styles.media}>
+
+                  {service.video ? (
+                    <video
+                      src={service.video}
+                      autoPlay
+                      loop
+                      className={styles.video}
+                    />
+                  ) : (
+
+                    <Image
+                      alt=""
+                      src={`/img/${service.photo}`}
+                      width="100%"
+                      height="100%"
+                      layout="responsive"
+                      objectFit="cover"
+                    />
+                  )}
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
